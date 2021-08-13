3ceb74dce89336cb59c0c02b2b02fbc3bd7512ea
Mohamed Abdelaziz Louhab <86206955+medAzizLHB@users.noreply.github.com>
2021-08-13 15:51:11 +0200

    Update ledBlinking.c

--- ledBlinking.c
+++ ledBlinking.c
@@ -1,3 +1,8 @@
+/*
+* medAzizLHB
+* SkorpTech
+* Blinking led in PC13 for 0.5 second
+*/
 void main() {
                 GPIO_Digital_Output(&GPIOC_BASE, _GPIO_PINMASK_13);
                 GPIOC_ODRbits.ODR13=1;
