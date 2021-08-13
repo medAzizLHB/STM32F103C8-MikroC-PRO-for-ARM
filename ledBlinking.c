f6e1107e68b234c2c6c9e1aba8e03c4803dbb056
Mohamed Abdelaziz Louhab <86206955+medAzizLHB@users.noreply.github.com>
2021-08-13 16:02:34 +0200

    Delete ledBlinking.c

--- ledBlinking.c
+++
@@ -1,9 +0,0 @@
-void main() {
-                GPIO_Digital_Output(&GPIOC_BASE, _GPIO_PINMASK_13);
-                GPIOC_ODRbits.ODR13=1;
-                
-                while(1) {
-                GPIOC_ODRbits.ODR13 = ~GPIOC_ODRbits.ODR13;
-                Delay_ms(500);
-                }
-}
